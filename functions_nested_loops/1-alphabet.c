301040b9835c015246f1c1d7f5874afe07b56e0c
lina_aziz <lenaexo0@gmail.com>
2025-10-03 00:12:29 +0300

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,6 +3,9 @@
 /**
  * print_alphabet - prints the alphabet in lowercase
  *
+ * Description: This function prints all lowercase letters
+ * from 'a' to 'z', followed by a new line.
+ *
  * Return: void
  */
 void print_alphabet(void)
