5cdef2bca1b49e002e0575e891761a9182fcf55c
lina_aziz <lenaexo0@gmail.com>
2025-10-03 02:33:17 +0300

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,21 +10,25 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
-	int started = 0;
-	int num = 0;
+	int result = 0;
 
-	while (s[i] != '\0')
+	/* skip non-digits, track sign */
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
-		{
-			if (!started)
-				sign = -sign;
-		}
-		else if (s[i] == '+')
-		{
-			/* ignore '+' before number */
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			started = 1;
-			if (sign == 1)
+			sign *= -1;
+		i++;
+	}
+
+	/* build value; build negative directly to handle INT_MIN */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (sign == 1)
+			result = result * 10 + (s[i] - '0');
+		else
+			result = result * 10 - (s[i] - '0');
+		i++;
+	}
+
+	return (result);
+}
