02cd782731592ac3d342478f370b8b6205e621bd
lina_aziz <lenaexo0@gmail.com>
2025-10-03 01:48:46 +0300

    Update 0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,13 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: the character to check (in ASCII code)
+ * @c: The character to check (ASCII code)
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    else
-        return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
