494e06efcd729b0eb56d53a07b64263adc2a9919
lina_aziz <lenaexo0@gmail.com>
2025-10-03 00:11:20 +0300

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase,
- * followed by a new line
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
  */
 void print_alphabet(void)
 {
