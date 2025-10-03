1388c431c52795177ea26b9f752d6be4e19fc994
lina_aziz <lenaexo0@gmail.com>
2025-10-03 04:57:54 +0300

    Create 3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of command-line arguments
+ * @argv: array of argument strings
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	product = a * b;
+
+	printf("%d\n", product);
+	return (0);
+}
