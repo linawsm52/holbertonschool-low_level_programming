bd07c6c31c14484bdb6db2f9a3c49ef178eb7862
lina_aziz <lenaexo0@gmail.com>
2025-10-03 03:20:24 +0300

    Create 7-print_chessboard.c

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints an 8x8 chessboard
+ * @a: array of 8 rows, each row has 8 chars
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
