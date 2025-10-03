ca0a1e462a29c8f06d6e3a175d2ee5e36fa41186
lina_aziz <lenaexo0@gmail.com>
2025-10-03 06:07:17 +0300

    Update main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,8 +1,13 @@
-/* main.h */
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Task 0 */
 void *malloc_checked(unsigned int b);
+
+/* Task 1 */
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif /* MAIN_H */
