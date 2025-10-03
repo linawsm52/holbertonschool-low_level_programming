cca8c4500420c9aee71c06b252cf7481c0bf7c0b
lina_aziz <lenaexo0@gmail.com>
2025-10-03 04:54:11 +0300

    Update 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,14 +2,13 @@
 
 /**
  * main - prints the program name, followed by a new line
- * @argc: number of command-line arguments
- * @argv: array of pointers to the arguments
+ * @argc: number of command-line arguments (unused)
+ * @argv: array of command-line argument strings
  *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void)argc;            /* silence unused-parameter warning */
 	printf("%s\n", argv[0]);
 	return (0);
 }
