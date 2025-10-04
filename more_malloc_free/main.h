11aed269b66757d043465fd33a98dc30cdc1761e
lina_aziz <lenaexo0@gmail.com>
2025-10-04 16:42:04 +0300

    Update main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,13 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
+int _putchar(char c);
 
-/* Task 0 */
+/* 0. malloc_checked */
 void *malloc_checked(unsigned int b);
 
-/* Task 1 */
+/* 1. string_nconcat */
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/* 2. _calloc */
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/* 3. array_range */
+int *array_range(int min, int max);
+
+/* 100. _realloc */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* MAIN_H */
