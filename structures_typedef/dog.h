ed3c00494867dcd682cb6e07d6a2b303e53e594b
lina_aziz <lenaexo0@gmail.com>
2025-10-04 17:05:18 +0300

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,8 +16,11 @@ struct dog
 	char *owner;
 };
 
+/* Typedef for struct dog */
+typedef struct dog dog_t;
+
+/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 
 #endif /* DOG_H */
