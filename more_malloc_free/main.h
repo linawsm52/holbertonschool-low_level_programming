fc02225d2c03626200195b17ad91590373ee2ab0
lina_aziz <lenaexo0@gmail.com>
2025-10-04 16:46:44 +0300

    Update main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -17,5 +17,7 @@ int *array_range(int min, int max);
 
 /* 100. _realloc */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
 
 #endif /* MAIN_H */
