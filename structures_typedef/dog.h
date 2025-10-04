3ba63d3d50f24c8da787759fba9d8905dfe2e491
lina_aziz <lenaexo0@gmail.com>
2025-10-04 16:55:00 +0300

    Create dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: Basic structure that stores a dog's information.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
