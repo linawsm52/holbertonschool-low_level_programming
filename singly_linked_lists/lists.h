132c6826b458c269823f54b81cac4aa91de26af2
lina_aziz <lenaexo0@gmail.com>
2025-10-09 15:31:02 +0300

    Update lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,34 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stddef.h> /* for size_t */
 
 /**
- * struct list_s - singly linked list
- * @str: string (malloc'ed)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
-
-size_t print_list(const list_t *h);
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stddef.h>  /* for size_t */
-
-/**
- * struct list_s - singly linked list
+ * struct list_s - singly linked list node
  * @str: string (malloc'ed)
  * @len: length of the string
  * @next: pointer to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
@@ -37,10 +18,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* Prototype required by the task */
+/* Prototypes used across the project (لا بأس تبقينها أو تحذفين غير اللازمة) */
 size_t list_len(const list_t *h);
-
-#endif /* LISTS_H */
-
 
 #endif /* LISTS_H */
