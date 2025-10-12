fb2b6a54264772ca2162e6412dd217ea35e6fb75
lina_aziz <lenaexo0@gmail.com>
2025-10-12 09:36:17 +0300

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,56 +6,50 @@
 
 #define BUFFER_SIZE 1024
 
-/**
- * error_exit - print message to STDERR and exit with code
- * @code: exit status
- * @fmt: printf-like format string
- * @arg: string argument used in the message (may be "")
- */
-static void error_exit(int code, const char *fmt, const char *arg)
+/* ---------- error helpers ---------- */
+static void die_usage(void)
 {
-	dprintf(STDERR_FILENO, fmt, arg);
-	exit(code);
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
 }
 
-/**
- * main - copy the content of a file to another file
- * @ac: argument count
- * @av: argument vector (av[1] = file_from, av[2] = file_to)
- *
- * Return: 0 on success, otherwise exits with codes:
- * 97 (usage), 98 (read error), 99 (write/create error), 100 (close error)
- */
-int main(int ac, char **av)
+static void die_read(const char *file)
 {
-	int fd_from, fd_to;
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	exit(98);
+}
+
+static void die_write(const char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(99);
+}
+
+static void die_close(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/* close wrapper that exits on failure (keeps main short & clean) */
+static void safe_close(int fd)
+{
+	if (close(fd) == -1)
+		die_close(fd);
+}
+
+/* ---------- copying logic (kept < 40 lines) ---------- */
+static void copy_fd(int fd_from, int fd_to, const char *from, const char *to)
+{
 	ssize_t r, w, done;
 	char buf[BUFFER_SIZE];
 
-	if (ac != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", "");
-
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", av[1]);
-
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close(fd_from);
-		error_exit(99, "Error: Can't write to %s\n", av[2]);
-	}
-
-	/* Always check read result immediately before any write */
 	for (;;)
 	{
+		/* check read() result immediately */
 		r = read(fd_from, buf, BUFFER_SIZE);
 		if (r == -1)
-		{
-			close(fd_from);
-			close(fd_to);
-			error_exit(98, "Error: Can't read from file %s\n", av[1]);
-		}
+			die_read(from);
 		if (r == 0) /* EOF */
 			break;
 
@@ -64,25 +58,34 @@ int main(int ac, char **av)
 		{
 			w = write(fd_to, buf + done, r - done);
 			if (w == -1)
-			{
-				close(fd_from);
-				close(fd_to);
-				error_exit(99, "Error: Can't write to %s\n", av[2]);
-			}
+				die_write(to);
 			done += w;
 		}
 	}
+}
 
-	if (close(fd_from) == -1)
+/* ---------- main (now short) ---------- */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to;
+
+	if (ac != 3)
+		die_usage();
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		die_read(av[1]);
+
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		safe_close(fd_from);
+		die_write(av[2]);
 	}
 
+	copy_fd(fd_from, fd_to, av[1], av[2]);
+
+	safe_close(fd_from);
+	safe_close(fd_to);
 	return (0);
 }
