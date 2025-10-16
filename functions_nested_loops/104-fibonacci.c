264703d3bc73e9db743c031affedff7953c09771
lina_aziz <lenaexo0@gmail.com>
2025-10-16 15:41:57 +0300

    Fix Fibonacci calculation for first 98 numbers
    Refactor Fibonacci calculation to handle larger numbers correctly and adjust initial values.

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,46 +2,62 @@
 
 /**
  * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
+ *
+ * Description: Numbers must be separated by comma and space,
+ * and printed using only printf, with correct precision
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long int i, j, k, j1, j2, k1, k2;
+	unsigned long int i, j, k;
+	unsigned long int i1, i2, j1, j2;
+	unsigned long int k1, k2;
 
-	i = 0;
-	j = 1;
+	i = 1;
+	j = 2;
 
 	for (k = 1; k <= 98; k++)
 	{
-		unsigned long int next;
-
-		next = i + j;
-		if (next < 10000000000)
+		if (i < 10000000000 && j < 10000000000)
 		{
-			printf("%lu", next);
+			if (k == 1)
+				printf("%lu, %lu", i, j);
 		}
-		else
+
+		if (k > 2)
 		{
-			j1 = i / 10000000000;
-			j2 = i % 10000000000;
-			k1 = j / 10000000000;
-			k2 = j % 10000000000;
-
-			j1 = j1 + k1;
-			j2 = j2 + k2;
-
-			if (j2 >= 10000000000)
+			if (i + j < 10000000000)
 			{
-				j1++;
-				j2 %= 10000000000;
+				printf(", %lu", i + j);
 			}
-
-			printf("%lu%010lu", j1, j2);
+			else
+			{
+				i1 = i / 10000000000;
+				i2 = i % 10000000000;
+				j1 = j / 10000000000;
+				j2 = j % 10000000000;
+				k1 = i1 + j1;
+				k2 = i2 + j2;
+				if (k2 >= 10000000000)
+				{
+					k1 += 1;
+					k2 %= 10000000000;
+				}
+				printf(", %lu%010lu", k1, k2);
+				i1 = j1;
+				i2 = j2;
+				j1 = k1;
+				j2 = k2;
+				i = 0;
+				j = 0;
+			}
 		}
-		i = j;
-		j = next;
-		if (k != 98)
-			printf(", ");
+		if (i + j < 10000000000)
+		{
+			j = i + j;
+			i = j - i;
+		}
 	}
 	printf("\n");
 	return (0);
