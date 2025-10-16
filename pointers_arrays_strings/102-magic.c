6f32f2be432c8b67f74880b2c30363b81725ef1d
lina_aziz <lenaexo0@gmail.com>
2025-10-16 16:21:56 +0300

    Update 102-magic.c

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,23 +1,51 @@
+#include "main.h"
 #include <stdio.h>
 
-int main(void)
+/**
+ * print_buffer - prints the content of a buffer
+ * @b: pointer to the buffer
+ * @size: number of bytes to print
+ *
+ * Each line shows:
+ * - the offset (8 hex chars)
+ * - 10 bytes of hex (2 chars each), grouped in pairs, separated by a space
+ * - the printable ASCII representation ('.' for non-printables)
+ * If size <= 0, prints only a newline.
+ */
+void print_buffer(char *b, int size)
 {
-    int n;
-    int a[5];
-    int *p;
+	int i, j;
 
-    a[2] = 1024;
-    p = &n;
-    /*
-     * write your line of code here...
-     * Remember:
-     * - you are not allowed to use a
-     * - you are not allowed to modify p
-     * - only one statement
-     * - you are not allowed to code anything else than this line of expected line of code
-     */
-    *(p + 5) = 98;
-    /* ...so that this prints 98\n */
-    printf("a[2] = %d\n", a[2]);
-    return (0);
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10; j++)
+		{
+			if (j % 2 == 0 && j != 0)
+				printf(" ");
+			if (i + j < size)
+				printf("%02x", (unsigned char)b[i + j]);
+			else
+				printf("  ");
+		}
+
+		printf(" ");
+
+		for (j = 0; j < 10 && i + j < size; j++)
+		{
+			unsigned char c = b[i + j];
+			if (c >= 32 && c <= 126)
+				printf("%c", c);
+			else
+				printf(".");
+		}
+		printf("\n");
+	}
 }
