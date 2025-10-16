4f44e66246a2c211babe5fe69a0acc76e570b548
lina_aziz <lenaexo0@gmail.com>
2025-10-16 16:02:03 +0300

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,5 +23,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-
+int _putchar(char c);
+char *rot13(char *s);
 #endif /* MAIN_H */
