8c234b389547f4a02334f09baf637bb74e90c6c1
lina_aziz <lenaexo0@gmail.com>
2025-10-16 15:42:48 +0300

    Optimize Fibonacci calculation and output
    Refactor Fibonacci sequence calculation to improve performance and readability. Adjust loop conditions and simplify printing logic.

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,62 +2,42 @@
 
 /**
  * main - prints the first 98 Fibonacci numbers, starting with 1 and 2
- *
- * Description: Numbers must be separated by comma and space,
- * and printed using only printf, with correct precision
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	unsigned long int i, j, k;
-	unsigned long int i1, i2, j1, j2;
-	unsigned long int k1, k2;
+	unsigned long int i1, i2, j1, j2, k1, k2;
 
 	i = 1;
 	j = 2;
 
-	for (k = 1; k <= 98; k++)
+	for (k = 1; k <= 91; k++)
 	{
-		if (i < 10000000000 && j < 10000000000)
+		printf("%lu, ", i);
+		j = j + i;
+		i = j - i;
+	}
+	i1 = i / 1000000000;
+	i2 = i % 1000000000;
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	for (; k <= 98; k++)
+	{
+		printf("%lu%09lu", j1, j2);
+		if (k != 98)
+			printf(", ");
+		k1 = i1 + j1;
+		k2 = i2 + j2;
+		if (k2 >= 1000000000)
 		{
-			if (k == 1)
-				printf("%lu, %lu", i, j);
+			k1 += 1;
+			k2 %= 1000000000;
 		}
-
-		if (k > 2)
-		{
-			if (i + j < 10000000000)
-			{
-				printf(", %lu", i + j);
-			}
-			else
-			{
-				i1 = i / 10000000000;
-				i2 = i % 10000000000;
-				j1 = j / 10000000000;
-				j2 = j % 10000000000;
-				k1 = i1 + j1;
-				k2 = i2 + j2;
-				if (k2 >= 10000000000)
-				{
-					k1 += 1;
-					k2 %= 10000000000;
-				}
-				printf(", %lu%010lu", k1, k2);
-				i1 = j1;
-				i2 = j2;
-				j1 = k1;
-				j2 = k2;
-				i = 0;
-				j = 0;
-			}
-		}
-		if (i + j < 10000000000)
-		{
-			j = i + j;
-			i = j - i;
-		}
+		i1 = j1;
+		i2 = j2;
+		j1 = k1;
+		j2 = k2;
 	}
 	printf("\n");
 	return (0);
