b351a4aeb8f945098f6872a036fb74a633ba7252
lina_aziz <lenaexo0@gmail.com>
2025-10-23 13:15:40 +0300

    Update 100-elf_header.c

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -19,6 +19,8 @@ void print_error(char *msg)
 	exit(98);
 }
 
+/* ------------------- BYTE ORDER HELPERS ------------------- */
+
 /**
  * rev16 - reverses bytes of a 16-bit value
  * @val: value to reverse
@@ -66,6 +68,8 @@ unsigned long rev64(unsigned long val)
 	}
 	return (res);
 }
+
+/* ------------------- PRINT HELPERS ------------------- */
 
 /**
  * print_magic - prints ELF magic bytes
@@ -132,23 +136,16 @@ void print_data(unsigned char *id)
 }
 
 /**
- * print_version - prints ELF version
+ * print_version_osabi - prints version and OS/ABI info
  * @id: e_ident array
  */
-void print_version(unsigned char *id)
+void print_version_osabi(unsigned char *id)
 {
 	printf("  Version:                           %d", id[EI_VERSION]);
 	if (id[EI_VERSION] == EV_CURRENT)
 		printf(" (current)");
 	printf("\n");
-}
 
-/**
- * print_osabi - prints OS/ABI
- * @id: e_ident array
- */
-void print_osabi(unsigned char *id)
-{
 	printf("  OS/ABI:                            ");
 	switch (id[EI_OSABI])
 	{
@@ -167,14 +164,7 @@ void print_osabi(unsigned char *id)
 	default:
 		printf("<unknown: %x>\n", id[EI_OSABI]);
 	}
-}
 
-/**
- * print_abi_version - prints ABI version
- * @id: e_ident array
- */
-void print_abi_version(unsigned char *id)
-{
 	printf("  ABI Version:                       %d\n", id[EI_ABIVERSION]);
 }
 
@@ -212,29 +202,63 @@ void print_type(unsigned short type, unsigned char *id)
 }
 
 /**
- * print_entry64 - prints 64-bit entry point
+ * print_entry - prints entry point address
  * @entry: entry address
  * @id: e_ident array
+ * @is64: 1 if ELF64, 0 if ELF32
  */
-void print_entry64(unsigned long entry, unsigned char *id)
+void print_entry(unsigned long entry, unsigned char *id, int is64)
 {
 	if (id[EI_DATA] == ELFDATA2MSB)
-		entry = rev64(entry);
-
+	{
+		if (is64)
+			entry = rev64(entry);
+		else
+			entry = rev32(entry);
+	}
 	printf("  Entry point address:               %#lx\n", entry);
 }
 
 /**
- * print_entry32 - prints 32-bit entry point
- * @entry: entry address
- * @id: e_ident array
+ * read_elf_header - reads and displays ELF header
+ * @fd: file descriptor
+ * @id: ELF identifier
  */
-void print_entry32(unsigned long entry, unsigned char *id)
+void read_elf_header(int fd, unsigned char *id)
 {
-	if (id[EI_DATA] == ELFDATA2MSB)
-		entry = rev32(entry);
+	ssize_t n;
 
-	printf("  Entry point address:               %#lx\n", entry);
+	if (id[EI_CLASS] == ELFCLASS32)
+	{
+		Elf32_Ehdr h32;
+
+		n = read(fd, &h32, sizeof(h32));
+		if (n != (ssize_t)sizeof(h32))
+		{
+			close(fd);
+			print_error("Error: Can't read ELF header");
+		}
+		print_type(h32.e_type, id);
+		print_entry((unsigned long)h32.e_entry, id, 0);
+	}
+	else if (id[EI_CLASS] == ELFCLASS64)
+	{
+		Elf64_Ehdr h64;
+
+		n = read(fd, &h64, sizeof(h64));
+		if (n != (ssize_t)sizeof(h64))
+		{
+			close(fd);
+			print_error("Error: Can't read ELF header");
+		}
+		print_type(h64.e_type, id);
+		print_entry((unsigned long)h64.e_entry, id, 1);
+	}
+	else
+	{
+		close(fd);
+		print_error("Error: Unknown ELF class");
+	}
 }
 
 /**
@@ -274,45 +298,10 @@ int main(int argc, char *argv[])
 	print_magic(id);
 	print_class(id);
 	print_data(id);
-	print_version(id);
-	print_osabi(id);
-	print_abi_version(id);
+	print_version_osabi(id);
 
 	lseek(fd, 0, SEEK_SET);
-
-	if (id[EI_CLASS] == ELFCLASS32)
-	{
-		Elf32_Ehdr h32;
-
-		n = read(fd, &h32, sizeof(h32));
-		if (n != (ssize_t)sizeof(h32))
-		{
-			close(fd);
-			print_error("Error: Can't read ELF header");
-		}
-
-		print_type(h32.e_type, id);
-		print_entry32((unsigned long)h32.e_entry, id);
-	}
-	else if (id[EI_CLASS] == ELFCLASS64)
-	{
-		Elf64_Ehdr h64;
-
-		n = read(fd, &h64, sizeof(h64));
-		if (n != (ssize_t)sizeof(h64))
-		{
-			close(fd);
-			print_error("Error: Can't read ELF header");
-		}
-
-		print_type(h64.e_type, id);
-		print_entry64((unsigned long)h64.e_entry, id);
-	}
-	else
-	{
-		close(fd);
-		print_error("Error: Unknown ELF class");
-	}
+	read_elf_header(fd, id);
 
 	close(fd);
 	return (0);
