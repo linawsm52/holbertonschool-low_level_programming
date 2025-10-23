344260e3e96f57c315d7a0ef5c74bc29bd78ce62
lina_aziz <lenaexo0@gmail.com>
2025-10-23 11:20:28 +0300

    Create 101-wildcmp.c

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and checks if they can be identical
+ * @s1: the first string
+ * @s2: the second string (can contain the special character *)
+ * Return: 1 if considered identical, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+	{
+		/* Skip multiple '*' or match any char sequence */
+		if (*(s2 + 1) == '\0')
+			return (1);
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+		return (0);
+	}
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
