352d4b74e15125231e40f7df98c87f6741b73304
lina_aziz <lenaexo0@gmail.com>
2025-10-23 11:28:52 +0300

    Update main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -21,4 +21,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
+int _isdigit(char *s);
+int _strlen(char *s);
+void errors(void);
+
 #endif /* MAIN_H */
