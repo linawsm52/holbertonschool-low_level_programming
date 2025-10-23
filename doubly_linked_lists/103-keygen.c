935b44526b27a0fae7d4b7827a0aef93bfceb3f3
lina_aziz <lenaexo0@gmail.com>
2025-10-23 12:48:31 +0300

    Update 103-keygen.c

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,13 +1,61 @@
-/* 103-keygen.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * main - generates a key depending on a username for crackme5
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, 1 on incorrect usage
+ * f4 - finds max char and returns randomized value
+ * @s: username
+ * @len: length of username
+ * Return: char for key[3]
+ */
+char f4(char *s, unsigned int len)
+{
+	unsigned long ch = 0;
+	unsigned int i;
+
+	for (i = 0; i < len; i++)
+		if ((unsigned long)s[i] > ch)
+			ch = (unsigned long)s[i];
+	srand((unsigned int)(ch ^ 14UL));
+	return ((char)(rand() & 63));
+}
+
+/**
+ * f5 - computes sum of squares
+ * @s: username
+ * @len: length
+ * Return: char for key[4]
+ */
+char f5(char *s, unsigned int len)
+{
+	unsigned long ch = 0;
+	unsigned int i;
+
+	for (i = 0; i < len; i++)
+		ch += (unsigned long)(s[i] * s[i]);
+	return ((char)((ch ^ 239UL) & 63));
+}
+
+/**
+ * f6 - random looped value
+ * @s: username
+ * Return: char for key[5]
+ */
+char f6(char *s)
+{
+	unsigned long ch = 0;
+	unsigned int i;
+
+	for (i = 0; i < (unsigned int)s[0]; i++)
+		ch = (unsigned long)rand();
+	return ((char)((ch ^ 229UL) & 63));
+}
+
+/**
+ * main - generates a valid key for crackme5
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 if usage error
  */
 int main(int argc, char *argv[])
 {
@@ -16,14 +64,10 @@ int main(int argc, char *argv[])
 	char *username;
 	char key[7];
 	unsigned long ops[] = {
-		0x3877445248432d41UL,
-		0x42394530534e6c37UL,
-		0x4d6e706762695432UL,
-		0x74767a5835737956UL,
-		0x2b554c59634a474fUL,
-		0x71786636576a6d34UL,
-		0x723161513346655aUL,
-		0x6b756f494b646850UL
+		0x3877445248432d41UL, 0x42394530534e6c37UL,
+		0x4d6e706762695432UL, 0x74767a5835737956UL,
+		0x2b554c59634a474fUL, 0x71786636576a6d34UL,
+		0x723161513346655aUL, 0x6b756f494b646850UL
 	};
 
 	if (argc != 2)
@@ -33,7 +77,7 @@ int main(int argc, char *argv[])
 	}
 
 	username = argv[1];
-	len = (unsigned int) strlen(username);
+	len = (unsigned int)strlen(username);
 
 	key[0] = (char)(((len ^ 59) & 63));
 
@@ -47,25 +91,12 @@ int main(int argc, char *argv[])
 		ch *= (unsigned long)username[i];
 	key[2] = (char)(((ch ^ 85UL) & 63));
 
-	ch = 0UL;
-	for (i = 0; i < len; i++)
-		if ((unsigned long)username[i] > ch)
-			ch = (unsigned long)username[i];
-	srand((unsigned int)(ch ^ 14UL));
-	key[3] = (char)(rand() & 63);
+	key[3] = f4(username, len);
+	key[4] = f5(username, len);
+	key[5] = f6(username);
 
-	ch = 0UL;
-	for (i = 0; i < len; i++)
-		ch += (unsigned long)(username[i] * username[i]);
-	key[4] = (char)(((ch ^ 239UL) & 63));
-
-	for (i = 0; i < (unsigned int)username[0]; i++)
-		ch = (unsigned long)rand();
-	key[5] = (char)(((ch ^ 229UL) & 63));
-
-	/* print the 6 characters by indexing the bytes of ops */
 	for (i = 0; i < 6; i++)
-		putchar(((char *)ops)[(unsigned int) key[i]]);
+		putchar(((char *)ops)[(unsigned int)key[i]]);
 	putchar('\n');
 
 	return (0);
