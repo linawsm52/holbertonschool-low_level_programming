3ea973b998e8eb69e9441c707d0c38aa4cc9f837
lina_aziz <lenaexo0@gmail.com>
2025-10-23 11:19:07 +0300

    Update main.h

--- recursion/main.h
+++ recursion/main.h
@@ -10,5 +10,8 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
+int _strlen_recursion(char *s);
+int check_pal(char *s, int start, int end);
+int is_palindrome(char *s);
 #endif /* MAIN_H */
 
