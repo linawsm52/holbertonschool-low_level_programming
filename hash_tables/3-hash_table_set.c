957c4f02d5aaf8485c1a007e19aae631a26eed95
lina_aziz <lenaexo0@gmail.com>
2025-10-23 10:57:07 +0300

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,40 @@
 #include "hash_tables.h"
 
 /**
+ * create_hash_node - Creates a new hash node
+ * @key: The key (cannot be an empty string)
+ * @value: The value associated with the key
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
+hash_node_t *create_hash_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
  * hash_table_set - Adds an element to the hash table
  * @ht: The hash table you want to add or update the key/value to
  * @key: The key (cannot be an empty string)
@@ -32,24 +66,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		current = current->next;
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = create_hash_node(key, value);
 	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
